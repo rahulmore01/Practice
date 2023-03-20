f22365a1f2705d30d1d68d9125cb7b41248c4f7e
Rahul Ashok More <98509088+rahulmore01@users.noreply.github.com>
2023-03-20 07:22:20 +0530

    Add files via upload
    this file content working file structure of react styled components

---
+++ CSS/my-styled-compo/src/App.js
@@ -0,0 +1,11 @@
+import {Container} from "./components/styles/Container.styled"
+
+function App() {
+  return (
+    <Container>
+      <h1>styled components</h1>
+    </Container>
+  );
+}
+
+export default App;
